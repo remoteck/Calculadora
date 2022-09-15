1c7ac0a9c2784e8c1f2899ba54380cf3ea2e1e04
Guilherme Perroni <guilhermegasparperroni97@gmail.com>
2022-09-15 17:37:56 -0300

    added inputs and subtraction with and without negative numbers

--- app/src/main/java/br/ggperroni/myapplication/MainActivity.kt
+++ app/src/main/java/br/ggperroni/myapplication/MainActivity.kt
@@ -3,15 +3,96 @@ package br.ggperroni.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
+import android.widget.TextView
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+    private var lastNumeric: Boolean = false
+    private var lastFloat: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Botão Pressionado", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastFloat = false
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
+    }
+
+    fun onFloat(view: View) {
+        if(lastNumeric && !lastFloat) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastFloat = true
+        }
+    }
+
+    fun onOperator(view: View) {
+        //checa se um número foi digitado e um operador foi digitado, se sim não é possível digitar mais um operador
+        tvInput?.text?.let {
+            if(!isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastFloat = false
+            }
+
+        }
+    }
+
+    fun onEqual(view: View) {
+        if(lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+
+            try {
+                if(tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if(tvValue.contains("-")) {
+
+                    //pega os 2 valores do input e separa em array
+                    val splitValue = tvValue.split("-")
+
+                    var firstValue = splitValue[0]
+                    val secondValue = splitValue[1]
+
+                    if(prefix.isNotEmpty()) {
+                        firstValue = prefix + firstValue
+                    }
+
+                    //realiza a subtração
+                    tvInput?.text = (firstValue.toDouble() - secondValue.toDouble()).toString()
+                }
+
+            } catch(e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        if(value.startsWith("-") && value.length == 1) {
+            return false
+        } else if(value.contains("/") || value.contains("*") || value.contains("+")) {
+            return true
+        } else if(value.length > 1) {
+            val temp = value.subSequence(1, value.length - 1)
+            return temp.contains("-")
+        } else {
+            return false
+        }
     }
 }
